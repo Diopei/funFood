ce2b3272ae6f06fd5608f3e1758dd9bb395d18d1
diopei <diopeitw@gmail.com>
2023-12-20 14:27:12 +0800

    [dev]安裝環境_設定Vue

---
+++ app/src/components/FunFoodHomePage.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FunFoodHomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
